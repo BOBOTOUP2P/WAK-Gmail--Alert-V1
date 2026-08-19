537124cf1cd7c87b5e0c1a35e17d7413f7c069ca
BOBOTOUP2P <nna617014@gmail.com>
2026-08-19 12:49:56 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// Firebase Config របស់អ្នក
 firebase.initializeApp({
   apiKey: "AIzaSyCK7w2phlBp-X6-1ycH4xvvIWuIv6FQH1A",
   authDomain: "my-first-app-babac.firebaseapp.com",
@@ -13,13 +12,12 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// បង្ហាញ Pop-up Notification ពេលបិទ Web
 messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title || "ទូទាត់ជោគជ័យ! 🎉";
   
   const notificationOptions = {
     body: payload.notification.body || "ប្រតិបត្តិការរបស់អ្នកទទួលបានជោគជ័យ។",
-    icon: "https://cdn-icons-png.flaticon.com/512/190/190411.png", // រូប Logo ខាងឆ្វេង
+    icon: "https://cdn-icons-png.flaticon.com/512/190/190411.png",
     vibrate: [200, 100, 200]
   };
 
